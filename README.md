1625a88f90e9533825c5ac12077dd9d1d167b4d5
Julia Rose Arenas <juliaarenas04@gmail.com>
2024-12-04 09:10:23 +0800

    update
    htaccess, database php file OK

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CycleSync
 CycleSync is a Period Tracker API
+
+dec 04 -- update
+htaccess
+database php connection
---
+++ config/database.php
@@ -0,0 +1,27 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=utf-8");
+    header("Access-Control-Allow-Methods: POST, GET, PATCH");
+    header("Access-Control-Max-Age: 3600");
+    date_default_timezone_set("Asia/Manila");
+
+    define("SERVER", "localhost");
+    define("DATABASE", "cycle_sync");
+    define("USER", "root");
+    define("PASSWORD", "");
+    define("TOKEN_KEY", ""); // hindi pa nalalagyan
+
+    class Connection{
+        protected $connectionString = "mysql:host" . SERVER . ";dbname=" .DATABASE. ";charset=utf8";
+        protected $options = [
+            \PDO::ATTR_ERRMODE =>\PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES => false
+        ];
+
+        public function connect(){
+            return new \PDO($this->connectionString,USER,PASSWORD,$this->options);
+        }
+    }
+
+?>
