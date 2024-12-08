4817d85c28af45694731625d1bc738182490ad8b
jxnnz <151096485+jxnnz@users.noreply.github.com>
2024-12-08 17:59:14 +0800

    Post.php

--- modules/Post.php
+++ modules/Post.php
@@ -7,33 +7,6 @@ class Post{
         $this->pdo = $pdo;
     }
 
-    public function postAccounts($body){
-        $values = [];
-        $errmsg = "";
-        $code = 0;
-
-        foreach($body as $value){
-            array_push($values, $value);
-        }
-
-        try{
-            $sqlString = "INSERT INTO accounts(userid, userName, email, password, token) VALUES (?,?,?,?,?)";
-            $sql = $this->pdo->prepare($sqlString);
-            $sql->execute($values);
-
-            $code = 200;
-            $data = null;
-
-            return array("data"=>$data, "code"=>$code);
-        }
-        catch (\PDOException $e){
-            $errmsg = $e->getMessage();
-            $code = 400;
-        }
-
-        return array("errmsg"=>$errmsg, "code"=>$code);
-    }
-
     public function postCycle($body){
         $values = [];
         $errmsg = "";
