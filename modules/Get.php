cb71fe5882b004e8a720136a8e10976f3ba0d589
jxnnz <151096485+jxnnz@users.noreply.github.com>
2024-12-09 22:57:58 +0800

    Update Get.php

--- modules/Get.php
+++ modules/Get.php
@@ -71,5 +71,100 @@ Class Get{
 
         return array("code" =>$code, "errmsg"=>$errmsg);
     }
+
+    public function getOvulation($id = null){
+        $sqlString = "SELECT * FROM ovulation_tbl";
+
+        if($id != null){
+            $sql .= "WHERE ovulationId = " .$id;
+        }
+        $data = array();
+        $errmsg = "";
+        $code = 0;
+
+        try{
+            if($result = $this->pdo->query($sqlString)->fetchAll()){
+                foreach($result as $record){
+                    array_push($data, $record);
+                }
+                $result = null;
+                $code = 200;
+                return array("code"=>$code, "data"=>$data);
+            }
+            else{
+                $errmsg = "No data found.";
+                $code = 404;
+            }
+        }
+        catch(\PDOException $e){
+            $errmsg = $e->getMessage();
+            $code = 403;
+        }
+        return array("code"=>$code, "errmsg"=>$errmsg);
+    }
+
+    public function getSymptom($id = null){
+        $sqlString = "SELECT * FROM symptom_tbl";
+
+        if($id != null){
+            $sql .= "WHERE symptomId = " .$id;
+        }
+        $data = array();
+        $errmsg = "";
+        $code = 0;
+
+        try{
+            if($result = $this->pdo->query($sqlString)->fetchAll()){
+                foreach($result as $record){
+                    array_push($data, $record);
+                }
+                $result = null;
+                $code = 200;
+                return array("code"=>$code, "data"=>$data);
+            }
+            else{
+                $errmsg = "No data found.";
+                $code = 403;
+            }
+        }
+        catch(\PDOException $e){
+            $errmsg = $e->getMessage();
+            $code = 403;
+        }
+        return array("code"=>$code, "errmsg"=>$errmsg);
+    }
+
+    public function getHealthMetric($id = null){
+        $sqlString = "SELECT * FROM health_metric";
+
+        if($id != null){
+            $sql .= "WHERE health_metric = " .$id;
+        }
+        $data = array();
+        $errmsg = "";
+        $code = 0;
+
+        try{
+            if($result = $this->pdo->query($sqlString)->fetchAll()){
+                foreach($result as $record){
+                    array_push($data, $record);
+                }
+                $result = null;
+                $code = 200;
+                return array("code"=>$code, "data"=>$data);
+            }
+            else{
+                $errmsg = "No data found.";
+                $code = 403;
+            }
+         }
+         catch(\PDOException $e){
+            $errmsg = $e->getMessage();
+            $code = 403;
+        }
+        return array("code"=>$code, "errmsg"=>$errmsg);
+     }
+
+     
 }
 ?>
